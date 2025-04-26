5baf29a9e24c895c2611929cdd00e86289d5871e
Parixyz <paribigdelian@gmail.com>
2025-04-26 03:37:14 -0400

    Update AccumlateDataCounters.py

--- AccumlateDataCounters.py
+++ AccumlateDataCounters.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import os
 
-# === Path ===
+
 base_dir = "C://VLNLP//Test"
 violation_file = os.path.join(base_dir, "violation_labels_updated.csv")
 
-# === Load CSV ===
+
 df = pd.read_csv(violation_file)
 
-# === Rule presence flags ===
+
 df['_rule1'] = df['violations'].str.contains('rule1', na=False).astype(int)
 df['_rule2'] = df['violations'].str.contains('rule2', na=False).astype(int)
 df['_rule3'] = df['violations'].str.contains('rule3', na=False).astype(int)
 
-# === Binary encoding: 2^x rule representation ===
+#  Binary encoding: 2^x to cover all varitions
 df['rule_code'] = df['_rule1'] * 1 + df['_rule2'] * 2 + df['_rule3'] * 4
 
-# === Print rule_code counts ===
+
 print(" Rule Code Distribution:")
 print(df['rule_code'].value_counts().sort_index())
 print(sum(df['rule_code'].value_counts()))
